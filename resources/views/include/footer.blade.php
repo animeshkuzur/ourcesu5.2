67df8a19ebdd731c69df1a1db4adef21d176c3ea
zurez <zurez4u@gmail.com>
2017-05-31 03:33:33 +0530

    added a way to add new subcat and pages

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -5,7 +5,7 @@
                     <div class="col-md-3 prd">
                         <h4>Ourcesu.com</h4>
                         <p><span class="glyphicon glyphicon-menu-right"></span> <a href="/page/262">Privacy Policy</a></p>
-                        <p><span class="glyphicon glyphicon-menu-right"></span> <a href="/page/267">Refund/Cancellation Policy</a></p>
+                        <p><span class="glyphicon glyphicon-menu-right"></span> <a href="/page/268">Refund & Cancellation Policy</a></p>
                       
                     </div>
                     <div class="col-md-3 prd">
